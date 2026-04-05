08ce9676c43cde525eced7074de85d3351598575
Godstime Sunday <sundaygodstimegt1@gmail.com>
2026-04-05 20:54:07 +0100

    modify: Update my resume

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -6,12 +6,14 @@ const skills = [
   "Framer Motion",
   "Git",
   "REST API",
+  "prisma orm",
+  "neon db",
 ];
 
 export default function Skills() {
   return (
     <section className="bg-[url('/bg-header-desktop.webp')] bg-cover bg-no-repeat px-4 py-20">
-      <div className="mx-auto max-w-4xl px-4">
+      <div className="mx-auto max-w-5xl px-4">
         <h2 className="mb-6 text-center text-3xl font-bold text-(--text-primary) md:text-left">
           Skills
         </h2>
